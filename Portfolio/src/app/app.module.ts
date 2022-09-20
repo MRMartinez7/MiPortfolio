835f87d4e4a653462e08928c1987463b36ced8fb
MRMartinez7 <100298380+MRMartinez7@users.noreply.github.com>
2022-09-20 12:37:09 -0300

    Portfolio
    proyecto integrador final

---
+++ Portfolio/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LogosComponent } from './components/logos/logos.component';
+import { SocialComponent } from './components/social/social.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { AboutmeComponent } from './components/aboutme/aboutme.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LogosComponent,
+    SocialComponent,
+    HeaderComponent,
+    BannerComponent,
+    AboutmeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
