3dab64b9819bffc3b29f1739c42fbdf8d0c54a2d
MRMartinez7 <100298380+MRMartinez7@users.noreply.github.com>
2022-09-22 14:52:33 -0300

    Estudios / H&S
    agregado

--- Portfolio/src/app/app.module.ts
+++ Portfolio/src/app/app.module.ts
@@ -10,6 +10,8 @@ import { BannerComponent } from './components/banner/banner.component';
 import { AboutmeComponent } from './components/aboutme/aboutme.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
 import { EstudiosComponent } from './components/estudios/estudios.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { SkillsComponent } from './components/skills/skills.component';
 
 @NgModule({
   declarations: [
@@ -20,11 +22,13 @@ import { EstudiosComponent } from './components/estudios/estudios.component';
     BannerComponent,
     AboutmeComponent,
     ExperienciaComponent,
-    EstudiosComponent
+    EstudiosComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({})
   ],
   providers: [],
   bootstrap: [AppComponent]
